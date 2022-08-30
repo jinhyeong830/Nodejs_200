65a49f673181a3fa9301ff0ad766abfc71acafad
Allie <shg02387@naver.com>
2022-08-30 11:43:31 +0900

    fs module 사용하기! : 123번~ 132번

--- fs_module.js
+++ fs_module.js
@@ -1,19 +1,40 @@
 /** @format */
 
+const fs = require('fs');
+/* 123. 파일리스트 출력*/
+const ms = 'txt 파일에 저장할 메세지';
+fs.writeFile('./textfile.txt', ms, 'utf-8', function (error) {
+  console.log('파일 쓰기 완료!');
+});
+
+/* 124. 동기로 파일 열기 */
+const data = fs.readFileSync('./textfile.txt');
+const string = data.toString();
+console.log(string);
+
+/* 125. 비동기로 파일 열기 */
+fs.readFile('./textfile.txt', (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+/* 128. 디렉토리 만들기 */
+const dirName = `${__dirname}/img`;
+
+if (!fs.existSync(dirName)) {
+  fs.mkdirSync(dirName);
+}
+
 /* 129. 파일 리스트 출력하기 */
 const testFolder = './'; //현재 디렉토리 의미
-const fs = require('fs');
-
 const filenameList = fs.readdirSync(testFolder); //함수의 인자로 디렉토리
 
 filenameList.forEach((fileName) => {
   console.log(fileName);
-});
-// 결과값으로 현재 파일에 있는 파일리스트들이 출력됨
+}); // 결과값으로 현재 파일에 있는 파일리스트들이 출력됨
 
 /* 130. object를  json형식으로 파일에 저장하기*/
 // JSON.stringify()
-
 const userList = [
   { name: 'Heewon', age: 28, gender: 'female', birthday: '4,June' },
   { name: 'Sangwoo', age: 27, gender: 'male', birthday: '30,August' },
@@ -26,11 +47,10 @@ console.log(typeof jsonUserList); //String
 
 // 현재파일에 list.json이라는 이름으로 파일 저장--> 예외처리 해주야함
 fs.writeFile('./list.json', jsonUserList, 'utf-8', function (error) {
-  console.log('파일 쓰기 작업이 끝났습니다!~');
+  console.log('파일 쓰기 작업완료!');
 });
 
 /* 131. 파일을 json형식으로 불러오기!*/
-
 fs.readFile('./list.json', (err, data) => {
   if (err) throw err;
   const friendList = JSON.parse(data.toString());
